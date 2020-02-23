20879c91746cf38794e8c61aab428c0554336751
Jason Racey <jasonracey@gmail.com>
2020-02-23 14:58:04 -0800

    Switch to Visual Studio project, rewrite implementation, add tests

---
+++ TopRepeatedNumber/TopRepeatedNumber.UnitTests/FindInTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TopRepeatedNumber.UnitTests
+{
+    [TestClass]
+    public class FindInTests
+    {
+        [TestMethod]
+        public void WhenNumbersIsNull_ThrowsArgumntNullException()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => TopRepeatedNumber.FindIn(null));
+        }
+
+        [TestMethod]
+        public void WhenNumbersIsEmpty_ThrowsArgumntNullException()
+        {
+            Assert.ThrowsException<ArgumentException>(() => TopRepeatedNumber.FindIn(Enumerable.Empty<int>()));
+        }
+
+        [TestMethod]
+        public void WhenNumbersContainsOneItem_ItemIsReturned_AndCountIsOne()
+        {
+            var numbers = new List<int> { 2 };
+
+            var result = TopRepeatedNumber.FindIn(numbers);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.Number);
+            Assert.AreEqual(1, result.Count);
+        }
+
+        [TestMethod]
+        public void WhenNumbersContainsMoreThanItem_AndOneItemIsTop_ReturnsItem_AndCount()
+        {
+            var numbers = new List<int> { 3, 2, 3 };
+
+            var result = TopRepeatedNumber.FindIn(numbers);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(3, result.Number);
+            Assert.AreEqual(2, result.Count);
+        }
+
+        [TestMethod]
+        public void WhenNumbersContainsMoreThanItem_AndMoreThanOneItemIsTop_ReturnsFirstItem_AndCount()
+        {
+            var numbers = new List<int> { 5, 7, 5, 4, 7, 7, 5 };
+
+            var result = TopRepeatedNumber.FindIn(numbers);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(5, result.Number);
+            Assert.AreEqual(3, result.Count);
+        }
+
+        [TestMethod]
+        public void WhenNumbersContainsMoreThanItem_AndNoItemsAreTop_ReturnsFirstItem_AndCount()
+        {
+            var numbers = new List<int> { 8, 7, 8, 4, 7 };
+
+            var result = TopRepeatedNumber.FindIn(numbers);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(8, result.Number);
+            Assert.AreEqual(2, result.Count);
+        }
+    }
+}
---
+++ TopRepeatedNumber/TopRepeatedNumber/TopRepeatedNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TopRepeatedNumber
+{
+    public static class TopRepeatedNumber
+    {
+        public static (int Number, int Count) FindIn(IEnumerable<int> numbers)
+        {
+            if (numbers == null) throw new ArgumentNullException(nameof(numbers));
+            if (!numbers.Any()) throw new ArgumentException("Collection must contain at least one item", nameof(numbers));
+
+            var counts = new Dictionary<int, int>();
+
+            foreach (var num in numbers)
+            {
+                if (!counts.ContainsKey(num)) counts.Add(num, 0);
+                counts[num]++;
+            }
+
+            var number = 0;
+            var count = 0;
+
+            foreach (var kvp in counts)
+            {
+                if (kvp.Value > count)
+                {
+                    number = kvp.Key;
+                    count = kvp.Value;
+                }
+            }
+
+            return (number, count);
+        }
+    }
+}
